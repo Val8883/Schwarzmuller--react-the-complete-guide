1460885a16d12868128d39ec1deb9e23e4ef6c41
Val Filippov <filippov.valentin@icloud.com>
2021-04-25 15:30:22 +0300

    added component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+
+export default function ExtenseItem({ title, id, amount, date }) {
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
